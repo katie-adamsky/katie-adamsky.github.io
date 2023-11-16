f3cc2bb411c149b9df813c1fbbe6244c74fd1334
Katie Adamsky <katieadamsky@gmail.com>
2023-11-16 14:58:55 -0800

    initial css files

---
+++ site/src/pages/projects/Project.js
@@ -0,0 +1,9 @@
+class Project {
+    constructor(title, summary, description, element) {
+        this.title=title;
+        this.summary=summary;
+        this.description=description;
+        this.element=element;
+    }
+}
+export default Project;
