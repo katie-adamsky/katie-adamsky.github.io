3dd13330ff25dca32ce953eeb4da8953f813629a
Katie Adamsky <katieadamsky@gmail.com>
2023-11-23 14:58:00 -0800

    make it purple, add image support from contentful

--- site/src/pages/projects/index.js
+++ site/src/pages/projects/index.js
@@ -1,17 +1,60 @@
 import {Routes, Route, Link} from 'react-router-dom';
+import { useState, useEffect } from "react";
 import ProjectPage from './ProjectPage';
 import projects from './project_list';
 import './projects.css';
 
+const query = `
+query {
+    projectThumbnailsCollection {
+      items{
+        projectName
+        thumbnail {
+          url
+        }
+      }
+    }
+    }
+`;
+
 function ProjectList({projects}) {
+    const [thumbnails, setThumbnails] = useState(null);
+  
+    useEffect(() => {
+      window
+        .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_DELIVERY_TOKEN}`,
+          },
+          body: JSON.stringify({ query }),
+        })
+        .then((response) => response.json())
+        .then(({ data, errors }) => {
+          if (errors) {
+            console.error(errors);
+          }
+  
+          setThumbnails(data?.projectThumbnailsCollection?.items);
+        });
+    }, []);
+    if (!thumbnails) {
+      return <div className="project-feed">"Loading..."</div>;
+    }
+
     return (
         <div className="project-feed">
-                {Object.entries(projects).map((project) => (
+                {Object.entries(projects).map((project) => {
+                console.log();
+                  const img = thumbnails.find(item => item.projectName===project[1].title);
+                  return (
                     <div key={project[0]} className="project-card">
-                        <h3><Link to={`${project[0]}`}>{project[1].title}</Link></h3>
-                        <p>{project[1].summary}</p>
+                      <img src={`${img?.thumbnail?.url}`} alt={`${img?.projectName}`}/>
+                      <h3><Link to={`${project[0]}`}>{project[1].title}</Link></h3>
+                      <p>{project[1].summary}</p>
                     </div>
-                ))}
+                );})}
         </div>
     );
 }
