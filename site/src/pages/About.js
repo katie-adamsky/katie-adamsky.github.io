6574b7a7c8d9229ae299c068dacb3a836fae6bab
Katie Adamsky <katieadamsky@gmail.com>
2023-11-28 17:11:40 -0800

    about me page

--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -5,44 +5,54 @@ import {
     faInstagram
   } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
+import options from './blog/options';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const About = () => {
+    const [author, setAuthor] = useState(null);
+    useEffect(() => {
+        const contentful = require('contentful')
+        const client = contentful.createClient({
+          space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+          accessToken: process.env.REACT_APP_CONTENTFUL_DELIVERY_TOKEN,
+        })
+  
+        client
+        .getEntries({
+          content_type: "componentAuthor",
+          limit: 1,
+          include: 10,
+        })
+        .then((entry) => setAuthor(entry.items))
+        .catch(console.error);
+    }, []);
+
     return (
+        <div className="page">
         <div className="about">
-            <h3>
-                About Me
-            </h3>
-            <p>
-                Hi! I'm Katie. I am a software developer looking for ways to explore my interests with code.
-                I am passionate about urban planning and active transportation, studying psychology and theosophy,
-                and automating the boring stuff.
-                Right now, I am making generative art with a metaphysical flavor.
-            </p>
-            <p>
-                You can check out the blog, which will mostly cover urbanism topics for now. It is powered by Contentful,
-                a delightful content management service.
-            </p>
-            <p>
-                You can also take a look at my p5 projects in the Projects tab.
-            </p>
-            <p>
-                When I'm not in front of the good screen, I am a volunteer librarian and a baritone singer in a barbershop chorus.
-            </p>
-            <div className="social-container">
-                <h3>Find me in cyberspace </h3>
-                <a href="https://www.github.com/katie-adamsky"
-                    className="social github">
-                    <FontAwesomeIcon icon={faGithubSquare} size="2x" />
-                </a>
-                <a href="https://www.linkedin.com/in/kathrynadamsky"
-                    className="social linkedin">
-                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                </a>
-                <a href="https://www.instagram.com/adarnsky"
-                    className="social insta">
-                    <FontAwesomeIcon icon={faInstagram} size="2x" />
-                </a>
+            <div className="circle-container">
+                <img className="katie" src={`${author?.[0].fields?.avatar?.fields?.file?.url}`} alt="katie-adamsky"/>
             </div>
+            <div className="bio">
+                {documentToReactComponents(author?.[0].fields?.bio, options)}
+            </div>
+        <div className="social-container">
+            <h3>Find me in cyberspace </h3>
+            <a href="https://www.github.com/katie-adamsky"
+                className="social github">
+                <FontAwesomeIcon icon={faGithubSquare} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/kathrynadamsky"
+                className="social linkedin">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/adarnsky"
+                className="social insta">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+        </div>
+        </div>
         </div>
     );
 };
